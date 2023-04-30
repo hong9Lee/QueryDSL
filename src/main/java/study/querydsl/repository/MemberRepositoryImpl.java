00a74f98625512be599c61e05459d01ef863ae08
hong9Lee <hongtemp9mac@gmail.com>
2023-04-30 22:08:11 +0900

    QuerydslRepositorySupport

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,56 +1,41 @@
 package study.querydsl.repository;
 
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.QTeam;
 
 import java.util.List;
 
-import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
 
-public class MemberRepositoryImpl implements MemberRepositoryCustom{
-    private final JPAQueryFactory queryFactory;
+public class MemberRepositoryImpl extends QuerydslRepositorySupport {
 
-    public MemberRepositoryImpl(JPAQueryFactory queryFactory) {
-        this.queryFactory = queryFactory;
+    public MemberRepositoryImpl() {
+        super(Member.class);
     }
 
-    @Override
-    public List<MemberTeamDto> search(MemberSearchCondition condition) {
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
-        if (hasText(condition.getUserName())) {
-            booleanBuilder.and(member.userName.eq(condition.getUserName()));
-        }
-
-        if (hasText(condition.getTeamName())) {
-            booleanBuilder.and(QTeam.team.name.eq(condition.getTeamName()));
-        }
-
-        if (condition.getAgeGoe() != null) {
-            booleanBuilder.and(member.age.goe(condition.getAgeGoe()));
-        }
-
-        if (condition.getAgeLoe() != null) {
-            booleanBuilder.and(member.age.goe(condition.getAgeLoe()));
-        }
-
-        return queryFactory
+    public List<MemberTeamDto> search(MemberSearchCondition condition, Pageable pageable) {
+        JPQLQuery<MemberTeamDto> jpaQuery = from(member)
+                .leftJoin(member.team, QTeam.team)
+                .where(member.userName.eq("asdf"))
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
                         member.userName,
                         member.age,
                         QTeam.team.id.as("teamId"),
                         QTeam.team.name.as("teamName")
-                ))
-                .from(member)
-                .leftJoin(member.team, QTeam.team)
-                .where(booleanBuilder)
-                .fetch();
+                ));
+
+        JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpaQuery);
+        return query.fetch();
+
     }
+
 
 }
